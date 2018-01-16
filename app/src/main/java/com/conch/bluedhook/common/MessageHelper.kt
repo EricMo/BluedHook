692f9586549605ea8229813770ca679b0d1ec3a8
EricMo <1101149089@qq.com>
2018-01-17 01:13:33 +0800

    notify text's color is too deep
    notify text's color is too deep.
    Optimize the the way to prevent recall message

--- app/src/main/java/com/conch/bluedhook/common/MessageHelper.kt
+++ app/src/main/java/com/conch/bluedhook/common/MessageHelper.kt
@@ -3,6 +3,8 @@ package com.conch.bluedhook.common
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.ContextCompat
 import android.view.Gravity
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -13,6 +15,7 @@ import android.widget.TextView
 object MessageHelper {
     /**
      * chat_notice_notify_content_style
+     * shape_common_round_graylighter_solid
      */
     @SuppressLint("ResourceType")
     fun makeNotifyTextView(mContext: Context, content: String): TextView {
@@ -22,7 +25,9 @@ object MessageHelper {
         params.gravity = Gravity.CENTER_HORIZONTAL
         notify.tag = 0x0000001
         notify.textSize = 12f
-        notify.setBackgroundResource(2131296712)
+        val shape = ContextCompat.getDrawable(mContext, 2130838772) as GradientDrawable
+        shape.setColor(Color.parseColor("#fff3f3f3"))
+        notify.setBackgroundDrawable(shape)
         notify.text = content
         notify.setTextColor(Color.parseColor("#ffadafb0"))
         notify.gravity = Gravity.CENTER_HORIZONTAL
